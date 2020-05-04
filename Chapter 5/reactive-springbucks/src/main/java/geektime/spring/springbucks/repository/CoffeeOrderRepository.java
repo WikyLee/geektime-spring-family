66402f8e589bdaec5bc7f85922b5f5c7d8fc4d42
liweijun6 <liweijun3@jd.com>
2020-05-04 12:13:48 +0800

    Revert "update r2dbc&h2"
    This reverts commit f2d73a5c7d095283df759adf718a6173f0732d0a.

--- Chapter 5/reactive-springbucks/src/main/java/geektime/spring/springbucks/repository/CoffeeOrderRepository.java
+++ Chapter 5/reactive-springbucks/src/main/java/geektime/spring/springbucks/repository/CoffeeOrderRepository.java
@@ -2,7 +2,7 @@ package geektime.spring.springbucks.repository;
 
 import geektime.spring.springbucks.model.CoffeeOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.r2dbc.core.DatabaseClient;
+import org.springframework.data.r2dbc.function.DatabaseClient;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
--- Chapter 5/reactive-springbucks/src/main/java/geektime/spring/springbucks/service/OrderService.java
+++ Chapter 5/reactive-springbucks/src/main/java/geektime/spring/springbucks/service/OrderService.java
@@ -3,7 +3,7 @@ package geektime.spring.springbucks.service;
 import geektime.spring.springbucks.model.CoffeeOrder;
 import geektime.spring.springbucks.repository.CoffeeOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.r2dbc.core.DatabaseClient;
+import org.springframework.data.r2dbc.function.DatabaseClient;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
